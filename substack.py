0045a8bceb676799419c2f03a10111ede24c48fc
everping <everping@outlook.com>
2016-12-06 16:46:37 +0700

    I made some big changes, especially I make the Profile class  to control options more effectively

--- substack.py
+++ substack.py
@@ -1,96 +1,16 @@
-import time
-from search.engine_factory import EngineFactory
-from data.domain import Domain
-from data.logger import logger
-from config import config
+from substack.data.profile import Profile
+from substack.substack_core import SubStack
 
 
-class SubStack:
-    def __init__(self):
-        self.already_walked = []
-        self.engines = []
-        self.start_time = 0
+def main():
+    profile = Profile("empty")
+    profile.set_target("bkav.com, garena.com")
 
-    def set_target(self, targets):
-        config.save("target", targets)
+    sub_stack = SubStack()
+    sub_stack.set_profile(profile)
 
-    def set_mode(self, modes):
-        if "discovery" in modes or "bruteforce" in modes:
-            config.load("mode")[mode]['active'] = True
-
-        if "discovery" not in modes:
-            config.load("mode")["discovery"]['active'] = False
-            config.load("mode")["discovery"]['engines'] = []
-
-        if "bruteforce" not in modes:
-            config.load("mode")["bruteforce"]['active'] = False
-
-    def set_engine(self, engines_name):
-        if config.load("mode")["discovery"]['active'] == True:
-            config.load("mode", "discovery")["engines"] = engines_name
-
-    def is_existed(self, sub_domain):
-        for domain in self.already_walked:
-            if domain.domain_name == sub_domain.domain_name:
-                return True
-        return False
-
-    def verify(self):
-        pass
-
-    def get_discovery_time(self):
-        now = time.time()
-        diff = now - self.start_time
-        return diff
-
-    def discover_and_brute_force(self):
-        targets = [Domain(domain_name) for domain_name in config.load("target")]
-        self.engines = [EngineFactory.create(engine_name) for engine_name in
-                        config.load("mode")['discovery']['engines']]
-
-        discovered_list = self.discover(targets)
-        return discovered_list
-
-    def discover(self, to_walk):
-        self.already_walked = to_walk
-
-        while len(to_walk):
-            sub_domain_list = []
-            for engine in self.engines:
-                for domain in to_walk:
-                    if self.get_discovery_time() > config.load("timeout"):
-                        logger.error("Stopped since overtime")
-                        break
-                    else:
-                        engine_result = engine.discover(domain)
-                        for i in engine_result:
-                            sub_domain_list.append(i)
-
-            new_domain = []
-
-            for domain in sub_domain_list:
-                if not self.is_existed(domain):
-                    new_domain.append(domain)
-                    self.already_walked.append(domain)
-                    logger.info('New domain found by %s: %s' % (domain.meta_data['found_by'], domain.domain_name))
-
-            to_walk = new_domain
-        return self.already_walked
-
-    def start(self):
-        logger.info("Start discovering sub-domains of %s" % ", ".join(config.load("target")))
-        self.start_time = time.time()
-        self.discover_and_brute_force()
+    sub_stack.start()
 
 
-domains = ['garena.com', 'bkav.com']
-mode = ["discovery", "bruteforce"]
-engines_name = ['Bing', 'Baidu']
-
-sub_stack = SubStack()
-
-sub_stack.set_target(domains)
-# sub_stack.set_mode(mode)
-# sub_stack.set_engine(engines_name)
-
-sub_stack.start()
+if __name__ == "__main__":
+    main()
---
+++ substack/substack_core.py
@@ -0,0 +1,84 @@
+import time
+
+from substack.data.domain import Domain
+from substack.data.logger import logger
+from substack.data.requester import Requester
+from substack.plugins.search.engine_factory import EngineFactory
+
+
+class SubStack:
+    def __init__(self):
+        self.already_walked = []
+        self.engines = []
+        self.start_time = 0
+        self.profile = None
+
+    def make_requester(self):
+        requester = Requester()
+        agent = self.profile.get_http_settings("agent")
+        proxy = self.profile.get_http_settings("proxy")
+
+        if agent is not None:
+            requester.set_agent(agent)
+
+        if proxy is not None:
+            requester.set_proxy(proxy)
+
+        return requester
+
+    def set_profile(self, profile):
+        self.profile = profile
+
+    def is_existed(self, sub_domain):
+        for domain in self.already_walked:
+            if domain.domain_name == sub_domain.domain_name:
+                return True
+        return False
+
+    def verify(self):
+        pass
+
+    def get_discovery_time(self):
+        now = time.time()
+        diff = now - self.start_time
+        return diff
+
+    def discover_and_brute_force(self):
+        targets = [Domain(domain_name) for domain_name in self.profile.get_target()]
+
+        for engine_name in self.profile.get_enabled_plugins("search"):
+            self.engines.append(EngineFactory.create(engine_name, self.make_requester()))
+
+        discovered_list = self.discover(targets)
+        return discovered_list
+
+    def discover(self, to_walk):
+        self.already_walked = to_walk
+
+        while len(to_walk):
+            sub_domain_list = []
+            for engine in self.engines:
+                for domain in to_walk:
+                    if self.get_discovery_time() > int(self.profile.get_misc_settings("max_discovery_time")):
+                        logger.error("Stopped since overtime")
+                        break
+                    else:
+                        engine_result = engine.discover(domain)
+                        for i in engine_result:
+                            sub_domain_list.append(i)
+
+            new_domain = []
+
+            for domain in sub_domain_list:
+                if not self.is_existed(domain):
+                    new_domain.append(domain)
+                    self.already_walked.append(domain)
+                    logger.info('New domain found by %s: %s' % (domain.meta_data['found_by'], domain.domain_name))
+
+            to_walk = new_domain
+        return self.already_walked
+
+    def start(self):
+        logger.info("Start discovering sub-domains of %s" % self.profile.get_target())
+        self.start_time = time.time()
+        self.discover_and_brute_force()
