2d7634754302ae3c84c620618d5c744f72be10dd
hoanhp <hoangphuhoan2012@gmail.com>
2016-12-26 00:42:38 +0700

    modify headers

--- substack/plugins/search/virustotal_plugin.py
+++ substack/plugins/search/virustotal_plugin.py
@@ -24,16 +24,17 @@ class VirustotalPlugin(SearchPlugin):
         return False
 
     def extract(self, url):
-        headers = {"Cookie":"VT_PREFERRED_LANGUAGE=vi; __utma=194538546.702379437.1482145535.1482145535.1482145535.1; __utmb=194538546.7.10.1482145535; __utmc=194538546; __utmz=194538546.1482145535.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1","User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+        headers = {"Cookie":"VT_PREFERRED_LANGUAGE=en; __utma=194538546.1669411933.1482685628.1482685628.1482685628.1; __utmb=194538546.1.10.1482685628; __utmc=194538546; __utmz=194538546.1482685628.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmt=1","User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0","Host":"www.virustotal.com"}
         content = self.requester.custom_get(url,headers).text
         if not self.has_error(content):
-            soup = BeautifulSoup(content)
+            soup = BeautifulSoup(content, "html5lib")
             search_tags = soup.find_all("a", attrs={"target":"_blank","class":None}, href = True)
             for tag in search_tags:
                 domain = tag.string
-                if domain is None:
-                    continue
-                else:
+                if domain is not None:
+                    domain = domain.strip()
                     self.add(domain)
+                else:
+                    break
         else:
             logger.error("Captcha_detected")
