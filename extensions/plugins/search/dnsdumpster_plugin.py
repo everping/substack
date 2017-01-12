9b39c0c5ad8d7d9be9f92aaa7f920df7dcf734e0
hoanhp <hoangphuhoan2012@gmail.com>
2017-01-12 23:17:24 +0700

    fix dnsdumpster_plugin

--- extensions/plugins/search/dnsdumpster_plugin.py
+++ extensions/plugins/search/dnsdumpster_plugin.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from substack.plugins.search_plugin import SearchPlugin
-
+from substack.data.logger import logger
 
 class DnsDumpsterPlugin(SearchPlugin):
     def __init__(self):
@@ -20,21 +20,21 @@ class DnsDumpsterPlugin(SearchPlugin):
         pass
 
     def extract(self, url):
+
         site = self.base_domain.domain_name
-        data = {
-            "csrfmiddlewaretoken": "xW6DIMrTX9qge6cQuCE1OmZgmGunVinw",
-            "targetip": site
-        }
 
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0",
-            "Cookie": "csrftoken=xW6DIMrTX9qge6cQuCE1OmZgmGunVinw",
-            "Referer": "https://dnsdumpster.com/"
-        }
+        header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0",
+                  "Cookie":"csrftoken=xW6DIMrTX9qge6cQuCE1OmZgmGunVinw",
+                  "Referer":"https://dnsdumpster.com/"}
+        payload = {"csrfmiddlewaretoken":"xW6DIMrTX9qge6cQuCE1OmZgmGunVinw",
+                   "targetip":site}
 
-        content = self.requester.post(url, headers=headers, data=data).text
-        soup = BeautifulSoup(content, "html5lib")
+        content = self.requester.post(url,payload,header).text
+        soup = BeautifulSoup(content,"lxml")
         search_tags = soup.find_all('td', attrs={"class": "col-md-4"})
+        if not search_tags:
+            logger.info("Plug-in DNsDumpster: can't get any result for: %s" % self.base_domain.domain_name)
+            return
         for tag in search_tags:
             domain = tag.contents[0].strip('.')
             if site in domain:
